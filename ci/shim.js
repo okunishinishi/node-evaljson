20bb6087e7cec917f6122c3956bf0bf106d1c327
okunishinishi <okunishinishi@gmail.com>
2018-08-09 16:24:51 +0900

    Version incremented to 6.0.1

--- ci/shim.js
+++ ci/shim.js
@@ -9,14 +9,14 @@
 process.chdir(`${__dirname}/..`)
 
 const { runTasks } = require('ape-tasking')
-const ababelES2015 = require('ababel-es2015')
+const ababel = require('ababel')
 
 runTasks('shim', [
-  () => ababelES2015('**/*.js', {
+  () => ababel('**/*.js', {
     cwd: 'lib',
     out: 'shim/node'
   }),
-  () => ababelES2015('**/*.js', {
+  () => ababel('**/*.js', {
     cwd: 'lib',
     out: 'shim/browser'
   })
